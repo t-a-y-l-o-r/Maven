df2f82b1385496348ec5ccb4c183d6867eef50c9
t-a-y-l-o-r <taylorjcochran@hotmail.com>
2023-05-17 14:13:06 -0700

    Fix control flow

--- django_command.pl
+++ django_command.pl
@@ -2,10 +2,11 @@
 
 use strict;
 use warnings;
+use Cwd;
 
 @ARGV == 2 or die "This script requires a project_prefix and a command_name\n";
 
-my $project_prefix = cwd() . $ARGV[0];
+my $project_prefix = getcwd() . "/" .$ARGV[0];
 my $command_name = $ARGV[1];
 
 defined $project_prefix or die "Project/app prefix is requied\n";
@@ -17,9 +18,18 @@ $command_name =~ /^[a-z_]+$/ or die "File name must be snake case!\n";
 -d $project_prefix . "/management" or mkdir $project_prefix . "/management" or die "Cant make managment folder\n";
 -d $project_prefix . "/management/commands" or mkdir $project_prefix . "/management/commands" or die "Cant make commands folder\n";
 
-open(my $file, ">", $project_prefix . "/management/__init__.py") or die "Failed to ensure init1 was created\n" and close $file;
-open(my $file, ">", $project_prefix . "/management/commands/__init__.py") or die "Failed to ensure init2 was created\n" and close $file;
+unless (-f $project_prefix . "/management/__init__.py") {
+  open(my $file_handle, ">", $project_prefix . "/management/__init__.py") or die "Failed to ensure init1 was created\n";
+  close $file_handle;
+}
+
+unless (-f $project_prefix . "/management/commands/__init__.py") {
+  open(my $file_handle, ">", $project_prefix . "/management/commands/__init__.py") or die "Failed to ensure init2 was created\n";
+  close $file_handle;
+}
 
 # the part that matters
-open(my $file, ">", $project_prefix . "/management/commands/" . $command_name .".py") or die "Failed to ensure command file was created\n" and close $file;
-
+unless (-f $project_prefix . "/management/commands/" . $command_name .".py") {
+  open(my $file_handle, ">", $project_prefix . "/management/commands/" . $command_name .".py") or die "Failed to ensure command file was created\n";
+  close $file_handle;
+}
