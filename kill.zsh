1093136db2516a5bee341fe23833a806d4d5c2e7
t-a-y-l-o-r <taylorjcochran@hotmail.com>
2023-05-23 09:31:03 -0700

    kill pi

---
+++ kill.zsh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+if [[ $# -eq 0 ]] ; then
+  echo 'Please provide a process name to search and kill.'
+  exit 1
+fi
+
+PROCESS_NAME=$1
+
+pgrep -fl "$PROCESS_NAME" | while read -r line ; do
+  PID=$(echo $line | cut -d ' ' -f1)
+  PROC_NAME=$(echo $line | cut -d ' ' -f2-)
+
+  echo "Found process: $PROC_NAME with PID: $PID"
+
+  read "resp?Do you want to kill this process? (y/n) "
+
+  if [[ "$resp" =~ ^[Yy]$ ]] ; then
+    kill -9 $PID
+    echo "Killed process $PID"
+  else
+    echo "Did not kill process $PID"
+    break
+  fi
+done
+
