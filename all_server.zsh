cd53fe490209db453c9c1ffe85a402dcdb4741c1
t-a-y-l-o-r <taylorjcochran@hotmail.com>
2023-05-23 09:28:42 -0700

    update server run

--- all_server.zsh
+++ all_server.zsh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 
+PROJECT_NAME=$(basename $(pwd))
+
 DJANGO_OUT=~/Desktop/django.output
 CELERY_OUT=~/Desktop/celery.output
 NPM_OUT=~/Desktop/npm.output
 
-django-admin runserver &> >(tee $DJANGO_OUT) &
-celery -A yourproject worker -l info &> >(tee $CELERY_OUT) &
-npm start &> >(tee $NPM_OUT) &
+django-admin runserver_plus &> >(tee $DJANGO_OUT) &
+poetry run celery -A $PROJECT_NAME.config worker -c 4 -l info &> >(tee $CELERY_OUT) & \
+  poetry run celery -A $PROJECT_NAME.config beat --schedule=/tmp/celerybeat-schedule
+npm run dev &> >(tee $NPM_OUT) &
