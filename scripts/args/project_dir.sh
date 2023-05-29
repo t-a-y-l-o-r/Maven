d4d819590bfc9fd46de1018438ecc02d7b51b0fe
t-a-y-l-o-r <taylorjcochran@hotmail.com>
2023-05-29 12:25:17 -0700

    source doesnt work in children

--- scripts/args/project_dir.sh
+++ scripts/args/project_dir.sh
@@ -4,4 +4,3 @@ cwd=$(pwd)
 escaped_cwd=$(printf '%s\n' "$cwd" | sed -e 's/[\/&]/\\&/g')
 
 sed -i "s|export MAVEN_PROJECT_DIR=.*|export MAVEN_PROJECT_DIR=\"$escaped_cwd\"|g" ~/.config/maven/rc
-source ~/.config/maven/rc
